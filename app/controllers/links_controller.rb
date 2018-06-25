c7368d56dc5403cc1cabf68d5f4272a4f89912b8
Nurtas <inurtas1999@gmail.com>
2018-06-25 10:47:14 +0600

    URL-Shortener

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,31 @@
+class LinksController < ApplicationController
+  def index
+    @links = Link.all
+    @links = @links.paginate(page: params[:page], per_page: 10)
+  end
+
+  def create
+    @link = Link.new(link_params)
+    respond_to do |format|
+	  	if @link.save
+	  			format.html {}
+	  			format.js {}
+	  	else
+	  			format.html {}
+	  			format.js {render "error" }
+	  	end
+	  end
+  end
+
+  def redirect
+    l = Link.find_by_short(params[:id])
+    l.update_attribute(:count, l.count + 1)
+    redirect_to l.url
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
+  end
+end
